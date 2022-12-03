f00fb0071c4d601e0a99522afbabe4861d2cb1d6
KingTimer12 <queentimer12@gmail.com>
2022-12-03 03:21:40 -0300

    fix undefined error

--- src/utils/googleApi.js
+++ src/utils/googleApi.js
@@ -3,10 +3,12 @@ require('dotenv').config()
 
 const service = google.sheets("v4");
 //const credentials = require("../../credentials.json");
+const private_key = process.env.PRIVATE_KEY
+console.log(private_key)
 const authClient = new google.auth.JWT(
     process.env.CLIENT_EMAIL,
     null,
-    process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+    private_key.replace(/\\n/g, "\n"),
     ["https://www.googleapis.com/auth/spreadsheets"]
 );
 const id = process.env.SPREADSHEETID
