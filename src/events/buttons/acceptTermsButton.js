38d55a9793afb68116638d5e181a09035d0e465c
KingTimer12 <queentimer12@gmail.com>
2023-04-20 20:53:14 -0300

    Segunda parte das perguntas finalizada

--- src/events/buttons/acceptTermsButton.js
+++ src/events/buttons/acceptTermsButton.js
@@ -3,6 +3,8 @@ const {
   ActionRowBuilder,
   StringSelectMenuBuilder,
   StringSelectMenuInteraction,
+  ButtonBuilder,
+  ButtonStyle,
 } = require("discord.js");
 const { emojis } = require("../../utils/emotes.json");
 const form = require("../../config/form.json");
@@ -11,14 +13,11 @@ const { addResponse, getResponses } = require("../../managers/formManager.js");
 let purpleHex = "#D000BA";
 
 async function sendMessage(interaction, question, { type, customId, options }) {
-  const embed = new EmbedBuilder()
-    .setColor(purpleHex)
-    .setTitle(question)
-    .setTimestamp();
+  
   let msg = undefined;
   let check = undefined;
   if (type == "writer") {
-    msg = await interaction.user.send({ embeds: [embed] });
+    msg = await interaction.user.send({ content: `**${question}**` });
     check = await awaitMessage(msg.channel);
   } else {
     let optionsWithEmoji = [];
@@ -40,7 +39,7 @@ async function sendMessage(interaction, question, { type, customId, options }) {
         .setPlaceholder("Selecione só uma opção")
         .setOptions(optionsWithEmoji)
     );
-    msg = await interaction.user.send({ embeds: [embed], components: [row] });
+    msg = await interaction.user.send({ content: `**${question}**`, components: [row] });
     check = await awaitComponent(msg, customId);
   }
   return check;
@@ -79,43 +78,92 @@ const addCacheAndNextQuestion = async (userId, responseCache) => {
   addResponse(userId, responseCache);
 };
 
+const whileResponses = async (interaction, userId, classification) => {
+  let index = 0;
+  while (true) {
+    const f = form[index];
+    if (f == undefined) break;
+    if (f.classification != classification) {
+      index++;
+      continue;
+    }
+    const response = await sendMessage(interaction, f.question, {
+      type: f.type,
+      customId: f.customId,
+      options: f.options,
+    });
+
+    if (response == undefined) break;
+
+    let responseCache = "";
+    if (response instanceof StringSelectMenuInteraction) {
+      await response.update({ fetchReply: true });
+      const value = response.values[0];
+      responseCache = value;
+    } else {
+      responseCache = response.content;
+    }
+
+    addCacheAndNextQuestion(userId, responseCache);
+
+    index++;
+  }
+};
+
 module.exports = {
   customId: "acceptTerms",
   async execute(interaction, client) {
     const { userId } = interaction;
     await interaction.deferUpdate().then(async () => {
-      const embed = new EmbedBuilder()
+      let embed = new EmbedBuilder()
         .setColor(purpleHex)
         .setTitle("Dados do Tutorando")
-        .setTimestamp();
       interaction.user.send({ embeds: [embed] });
 
-      let index = 0;
-      let f = form[index];
-      while (f != undefined) {
-        f = form[index];
-        if (f.classification != "data") break;
-        const response = await sendMessage(interaction, f.question, {
-          type: f.type,
-          customId: f.customId,
-          options: f.options,
-        });
+      await whileResponses(interaction, userId, "data");
 
-        if (response == undefined) break;
+      const tutoriaPlus = new EmbedBuilder()
+        .setColor(purpleHex)
+        .setTitle("Tutorando+")
+        .setDescription(
+          `Com apenas R$5, você pode se tornar um Tutorando+ dentro da Novel Brasil. Tutorando+ é só uma forma de ajudar o servidor a fazer mais eventos, mais divulgação, novos membros, para a melhora do ambiente do server. \n\nAlém de ajudar o servidor, com essa assinatura mensal você pode garantir os seguintes bônus: \n\n> O tutorando+ tem uma aula extra por semana! Você pode pedir mais uma aula para o seu tutor, basta marcar com ele; \n> Cadeira em roteirismo dentro do currículo em todas as Classes (apesar de tocarmos levemente no assunto "roteiro" no currículo comum, não tem a mesma profundidade da cadeira liberada para o tutorando+).\n\n**Deseja ser um tutorando+?**`
+        );
+      const next = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setCustomId("stepTwoForm-yes")
+          .setEmoji({ id: "1051884168977584139", name: "ready" })
+          .setLabel("Sim")
+          .setStyle(ButtonStyle.Success),
+        new ButtonBuilder()
+          .setCustomId("stepTwoForm-nop")
+          .setEmoji({ id: "1051884167782219776", name: "error" })
+          .setLabel("Não")
+          .setStyle(ButtonStyle.Danger)
+      );
+      const msgNext = await interaction.user.send({
+        embeds: [tutoriaPlus],
+        components: [next],
+      });
+      const filter = (interaction) =>
+        interaction.customId.split("-")[0] === "stepTwoForm";
+      const interactionResult = await msgNext
+        .awaitMessageComponent({
+          filter,
+          time: 600_000,
+          errors: ["time"],
+        })
+        .catch(console.error);
+      if (interactionResult == undefined) return;
+      await interactionResult.update({fetchReply: true})
+      const responseStepTwoForm = interactionResult.customId.split("-")[1];
+      addResponse(userId, responseStepTwoForm === "yes" ? "Sim" : "Não");
 
-        let responseCache = "";
-        if (response instanceof StringSelectMenuInteraction) {
-          await response.update({ fetchReply: true });
-          const value = response.values[0];
-          responseCache = value;
-        } else {
-          responseCache = response.content;
-        }
+      embed = new EmbedBuilder()
+        .setColor(purpleHex)
+        .setTitle("Perguntas para avaliação de conhecimento")
+      interaction.user.send({ embeds: [embed] });
+      await whileResponses(interaction, userId, "knowledge");
 
-        addCacheAndNextQuestion(userId, responseCache);
-
-        index++;
-      }
       
     });
   },
