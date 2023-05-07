4969ed76b6be258818fbc1e6b7f229b26faed88c
KingTimer12 <queentimer12@gmail.com>
2023-05-07 00:43:08 -0300

    Aplicação da OpenAPI na Nebi

--- src/events/messageCreateEvent.js
+++ src/events/messageCreateEvent.js
@@ -1,3 +1,4 @@
+const { Configuration, OpenAIApi } = require("openai");
 const {
   getUser,
   addUser,
@@ -5,6 +6,14 @@ const {
   addCooldown,
 } = require("../database/handler/userHandler");
 const { hasUser, saveUser } = require("../database/manager/userManager");
+const { ask } = require("../utils/aiApi");
+
+require("dotenv").config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 module.exports = {
   name: "Message Create",
@@ -13,6 +22,18 @@ module.exports = {
 
   async createEvent(message) {
     if (message.author.bot) return;
+
+    if (
+      message.channel.id === "1046080830075834392" &&
+      message.content.startsWith("!")
+    ) {
+
+      await message.channel.sendTyping()
+
+      const prompt = message.content.substring(1);
+      const answer = await ask(prompt);
+      message.reply(answer)
+    }
 
     /*const { user } = message.member;
     const userId = user.id;
---
+++ src/utils/aiApi.js
@@ -0,0 +1,20 @@
+//In ai.js
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
+async function ask(prompt) {
+  const response = await openai.createCompletion({
+    prompt,
+    model: "text-davinci-003",
+    max_tokens: 2500,
+    temperature: 0.3,
+    top_p: 0.3,
+    presence_penalty: 0,
+    frequency_penalty: 0.5,
+  });
+  return response.data.choices[0].text;
+}
+
+module.exports = {ask};
