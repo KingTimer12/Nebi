5f6cbe240bfaba6d7e8402e07f242607e0be16fd
KingTimer12 <queentimer12@gmail.com>
2023-05-08 04:02:51 -0300

    NebiAI é real

--- src/utils/aiApi.js
+++ src/utils/aiApi.js
@@ -1,18 +1,24 @@
 //In ai.js
+
+const nebi = require('./../config/NebiAI.json')
+
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
   apiKey: process.env.OPEN_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 async function ask(prompt) {
+
+  
+
   const response = await openai.createCompletion({
-    prompt,
-    model: "text-davinci-003",
-    max_tokens: 2500,
-    temperature: 0.3,
-    top_p: 0.3,
-    presence_penalty: 0,
-    frequency_penalty: 0.5,
+    prompt: nebi.OAI_data[0].prompt + "\n\n#########\n" + prompt + "\n#########\n",
+    model: nebi.OAI_data[0].model,
+    max_tokens: nebi.OAI_data[0].max_tokens,
+    temperature: nebi.OAI_data[0].temperature,
+    top_p: nebi.OAI_data[0].top_p,
+    presence_penalty: nebi.OAI_data[0].presence_penalty,
+    frequency_penalty: nebi.OAI_data[0].frequency_penalty
   });
   return response.data.choices[0].text;
 }
