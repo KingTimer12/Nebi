7069dc5d51d0f3113c2194a2e5ea70147543684e
KingTimer12 <queentimer12@gmail.com>
2023-09-11 00:12:02 -0300

    Atualizei só pra fazer essa bomba funcionar

--- src/managers/drawCheckManager.js
+++ src/managers/drawCheckManager.js
@@ -18,7 +18,7 @@ const checkingDraw = async (guild, drawChannel) => {
   } else if (
     currentDate.weekday() == 1 &&
     currentDate.hours() == 0 &&
-    currentDate.minutes() == 0
+    currentDate.minutes() == 15
   ) {
     await resetUserDraw();
     console.log("[DrawEvent] Update event for next sunday.");
